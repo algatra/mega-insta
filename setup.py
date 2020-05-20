c716b4bfbdd4e5c1d4fb2de89bffad85ce377f4c
Megatruh <55190771+algatra@users.noreply.github.com>
2020-05-21 02:12:38 +0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 import os
 
 os.system('pip install selenium')
-os.system('pip install requests')
+os.system('clear')
 os.system('pip install tqdm')
+os.system('clear')
+os.system('pip install requests')
+os.system('clear')
+
+os.system('pip3 install selenium')
+os.system('clear')
+os.system('pip3 install tqdm')
+os.system('clear')
+os.system('pip3 install requests')
+os.system('clear')
